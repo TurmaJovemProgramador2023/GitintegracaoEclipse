34c2095ce834bfdf8f1445394e625479da9b1df4
TurmaJovemProgramador2023 <jovemprogramador2023@gmail.com>
2025-07-24 16:17:11 -0300

    segundo commit

--- ProjetoIntegracaoGitEclipeGitHub/src/TesteintegracaoGit/TesteIntegracao.java
+++ ProjetoIntegracaoGitEclipeGitHub/src/TesteintegracaoGit/TesteIntegracao.java
@@ -5,6 +5,7 @@ public class TesteIntegracao {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Primeiro teste");
+		System.out.println("segundo teste");
 
 	}
 
